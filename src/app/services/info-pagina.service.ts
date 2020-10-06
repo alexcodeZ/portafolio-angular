720bc0668f4075dc91330b401e4b9ae682c45674
Alexander Tello <atelloquispe@gmail.com>
2020-10-06 10:02:25 -0500

    Fin sección 7

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { InforPagina } from '../interfaces/info-pagina.interface';
+import { Producto } from '../interfaces/producto.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -8,15 +9,25 @@ import { InforPagina } from '../interfaces/info-pagina.interface';
 export class InfoPaginaService {
   info: InforPagina = {};
   cargada = false;
+  equipo:any [] = [];
 
   constructor(private http: HttpClient) { 
-    console.log("Servicio de info cargado");
 
+   this.cargarInfo();
+   this.CargarEquipo();
+  }
+
+  private cargarInfo(){
     this.http.get('assets/data/data-pagina.json')
     .subscribe((resp: InforPagina) =>{
       this.cargada = true;
       this.info = resp;
-      console.log(resp.email);
+    })
+  }
+  private CargarEquipo(){
+    this.http.get('https://angular-html-2dd01.firebaseio.com/equipo.json')
+    .subscribe((resp: Producto[]) =>{
+      this.equipo = resp;
     })
   }
 }
